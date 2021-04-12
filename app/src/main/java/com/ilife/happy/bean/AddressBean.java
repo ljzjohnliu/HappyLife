4bbbe971004dd39011c865dc22f8e70f3ad600f7
zhengdan_nacy <www.baobeinet@163.com>
2021-04-12 18:02:44 +0800

    根据谷歌api定位

---
+++ app/src/main/java/com/ilife/happy/bean/AddressBean.java
@@ -0,0 +1,22 @@
+package com.ilife.happy.bean;
+
+public class AddressBean {
+    private double longitude;
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
