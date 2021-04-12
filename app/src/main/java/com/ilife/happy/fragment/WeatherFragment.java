edbfa59309c30613ff986552764276208443ce43
zhengdan_nacy <www.baobeinet@163.com>
2021-04-12 18:02:44 +0800

    根据谷歌api定位

--- app/src/main/java/com/ilife/happy/fragment/WeatherFragment.java
+++ app/src/main/java/com/ilife/happy/fragment/WeatherFragment.java
@@ -5,21 +5,39 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 
 import com.ilife.common.basemvp.BaseFragment;
 import com.ilife.happy.R;
+import com.ilife.happy.bean.AddressBean;
 import com.ilife.happy.bean.WeatherInfo;
 import com.ilife.happy.contract.IWeatherContract;
 import com.ilife.happy.presenter.WeatherPresenter;
 import com.ilife.happy.utils.IntentUtil;
 import com.ilife.happy.utils.SettingUtils;
+
+import android.location.Geocoder;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class WeatherFragment extends BaseFragment<WeatherPresenter, IWeatherContract.View> {
     public static String TAG = "WeatherFragment";
@@ -27,6 +45,31 @@ public class WeatherFragment extends BaseFragment<WeatherPresenter, IWeatherCont
     String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
 
     private WeatherPresenter mWeatherPresenter;
+
+    private Button mGpsBtn;
+    private TextView mGpsTxt;
+
+    private Handler mHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            AddressBean mAddressBean = (AddressBean) msg.obj;
+            double longitude = mAddressBean.getLongitude();// 经度
+            double latitude = mAddressBean.getLatitude();// 纬度
+            Log.d(TAG, "handleMessage:   longitude   " + longitude + " latitude   " + latitude);
+            Geocoder geocoder = new Geocoder(getContext(), Locale.getDefault());
+            List<Address> result = null;
+            try {
+                result = geocoder.getFromLocation(latitude, longitude, 1);
+                if(result != null){
+                    Log.d(TAG, "handleMessage:   result   " + result.get(0));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    };
 
     @Override
     public IWeatherContract.View getContract() {
@@ -53,7 +96,20 @@ public class WeatherFragment extends BaseFragment<WeatherPresenter, IWeatherCont
 
     @Override
     protected void initView() {
+        mGpsBtn = mRootView.findViewById(R.id.gps_btn);
+        mGpsTxt = mRootView.findViewById(R.id.gps_txt);
 
+        mGpsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: isLocationServiceEnabled " + isLocationServiceEnabled());
+                if (isLocationServiceEnabled()) {
+                    getLocation();
+                } else {
+                    Toast.makeText(getContext(), "定位失败", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -67,6 +123,66 @@ public class WeatherFragment extends BaseFragment<WeatherPresenter, IWeatherCont
         }
     }
 
+
+    public boolean isLocationServiceEnabled() {
+        LocationManager mLocationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        boolean networkEnable = mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean gpsEnable = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean passiveEnable = mLocationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER);
+        boolean isable = false;
+        if (networkEnable || gpsEnable) {
+            isable = true;
+        } else {
+            isable = false;
+        }
+        return isable;
+    }
+
+    public void getLocation() {
+        LocationManager mLocationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        Criteria mCriteria = new Criteria();
+        mCriteria.setAltitudeRequired(false);
+        mCriteria.setBearingRequired(false);
+        mCriteria.setCostAllowed(false);
+        mCriteria.setPowerRequirement(Criteria.POWER_LOW);
+        mCriteria.setAccuracy(Criteria.ACCURACY_COARSE);
+
+        String providerName = "";
+        List<String> providerList = mLocationManager.getProviders(true);
+        if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            providerName = LocationManager.NETWORK_PROVIDER;
+        } else if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            providerName = LocationManager.GPS_PROVIDER;
+        } else {
+            if (ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getContext(), "权限未授权，请先授权UHello定位权限", Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+
+
+        Location location = mLocationManager.getLastKnownLocation(providerName);
+        if (location != null) {
+            double longitude = location.getLongitude();// 经度
+            double latitude = location.getLatitude();// 纬度
+
+            Log.d(TAG, "longitude = " + longitude);
+            Log.d(TAG, "latitude = " + latitude);
+            mGpsTxt.setText("经度 " + longitude + "   纬度  " + latitude);
+            Message message = new Message();
+            AddressBean bean = new AddressBean();
+            bean.setLongitude(longitude);
+            bean.setLatitude(latitude);
+            message.obj = bean;
+            mHandler.sendMessage(message);
+
+        }
+
+    }
 
     @Override
     public void onResume() {
@@ -107,5 +223,11 @@ public class WeatherFragment extends BaseFragment<WeatherPresenter, IWeatherCont
                     })
                     .show();
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
