e823eec97ee0850110b13719bb115ddf50ef7500
zhengdan_nacy <www.baobeinet@163.com>
2021-05-24 12:51:17 +0800

    cirle view complete

---
+++ app/src/main/java/com/ilife/happy/activity/TestCustomViewActivity.java
@@ -0,0 +1,16 @@
+package com.ilife.happy.activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ilife.happy.R;
+
+public class TestCustomViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_view);
+    }
+}
--- app/src/main/java/com/ilife/happy/fragment/MineFragment.kt
+++ app/src/main/java/com/ilife/happy/fragment/MineFragment.kt
@@ -1,9 +1,14 @@
 package com.ilife.happy.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 import com.ilife.common.basemvp.BaseFragment
 import com.ilife.happy.R
+import com.ilife.happy.activity.CenterFabActivity
+import com.ilife.happy.activity.TestCustomViewActivity
 import com.ilife.happy.bean.UserInfo
 import com.ilife.happy.contract.IMineContract
 import com.ilife.happy.presenter.MinePresenter
@@ -39,13 +44,11 @@ class MineFragment : BaseFragment<MinePresenter?, IMineContract.View<*>?>() {
     }
 
     override fun initView() {
-//        val textView = mRootView.findViewById<View>(R.id.fragment_text_view) as TextView
-//        var bundle = getArguments()
-//        var s = bundle!!.getString("args")
-//        textView.setOnClickListener(View.OnClickListener {
-//            getPresenter().contract.personApi("arg1", "arg2")
-//        })
-//        textView.text = s
+        val textView = mRootView.findViewById<View>(R.id.test_btn) as TextView
+        textView.setOnClickListener(View.OnClickListener {
+            val intent = Intent(mContext, TestCustomViewActivity::class.java)
+            startActivity(intent)
+        })
     }
 
     override fun initData() {
